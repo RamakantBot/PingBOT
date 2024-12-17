c6b2ae5c90fa96cf50a281c4c6be47cd47b08352
Niraj Narwade <141415966+Niraj75678@users.noreply.github.com>
2024-12-17 20:42:49 +0530

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,14 @@
 module.exports = {
   name: "ping",
-  description: "Replies with Pong!",
+  description: "PING YO MAMA",
   execute(message, args) {
-    message.reply("Pong!");
+    
+const user = message.mentions.users.first();
+    
+    if (user) {      
+ message.channel.send(`${user} namaskar 🙏`);
+     } else {
+  message.channel.send("Kya hai?");
+    }
   },
 };
