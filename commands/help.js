84d2c527e301dc151834da4a0c1886e086470c22
Niraj Narwade <141415966+Niraj75678@users.noreply.github.com>
2024-12-18 13:50:21 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,8 +4,8 @@ module.exports = {
   name: "help",
   description: "Madat karnari command",
   async execute(message, args) {
-    const prefixCommands = message.client.commands.map(cmd => `\`${cmd.name}\``).join(", ");
-    const slashCommands = message.client.slashCommands.map(cmd => `\`/${cmd.data.name}\``).join(", ");
+    const prefixCommands = message.client.commands.map(cmd => `\`\`\`${cmd.name}\`\`\``).join(", ");
+    const slashCommands = message.client.slashCommands.map(cmd => `\`\`\`/${cmd.data.name}\`\`\``).join(", ");
 
     const embed = new EmbedBuilder()
       .setColor("#87ceeb")
