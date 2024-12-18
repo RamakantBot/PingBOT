def40411826bbe5e644b97f2c179ec52d34442dd
Niraj Narwade <141415966+Niraj75678@users.noreply.github.com>
2024-12-18 13:45:35 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,12 @@ module.exports = {
   name: "help",
   description: "Madat karnari command",
   async execute(message, args) {
-    const prefixCommands = message.client.commands.map(cmd => `\`${cmd.name}\`: ${cmd.description || "JAANKARI UPLABDH NAHI HAI"}`).join("\n");
-    const slashCommands = message.client.slashCommands.map(cmd => `\`/${cmd.data.name}\`: ${cmd.data.description || "JAANKARI UPLABDH NAHI HAI"}`).join("\n");
+    const prefixCommands = message.client.commands.map(cmd => `\`${cmd.name}\``).join(", ");
+    const slashCommands = message.client.slashCommands.map(cmd => `\`/${cmd.data.name}\``).join(", ");
 
     const embed = new EmbedBuilder()
       .setColor("#87ceeb")
       .setTitle("HELP SECTION")      
-      .setDescription("list of all commands")
       .addFields(
         { name: "Prefix Commands", value: prefixCommands || "No prefix commands available.", inline: false },
         { name: "Slash Commands", value: slashCommands || "No slash commands available.", inline: false }
