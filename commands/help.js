80e2b60f80edac913c9479f1edd3eda20a00bd51
Niraj Narwade <141415966+Niraj75678@users.noreply.github.com>
2024-12-18 13:51:09 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
       .addFields(
         { name: "Prefix Commands", value: prefixCommands || "No prefix commands available.", inline: false },
         { name: "Slash Commands", value: slashCommands || "No slash commands available.", inline: false }
-      )
+      );
       
     message.channel.send({ embeds: [embed] });
   },
