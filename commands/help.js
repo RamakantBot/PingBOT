5f19090359548866e018e05476fbfbff865115e2
Niraj Narwade <141415966+Niraj75678@users.noreply.github.com>
2024-12-18 13:55:35 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,15 +4,15 @@ module.exports = {
   name: "help",
   description: "Madat karnari command",
   async execute(message, args) {
-    const prefixCommands = message.client.commands.map(cmd => `\`\`\`${cmd.name}\`\`\``).join(", ");
-    const slashCommands = message.client.slashCommands.map(cmd => `\`\`\`/${cmd.data.name}\`\`\``).join(", ");
+    const prefixCommands = message.client.commands.map(cmd => `${cmd.name}`).join(", ");
+    const slashCommands = message.client.slashCommands.map(cmd => `${cmd.data.name}`).join(", ");
 
     const embed = new EmbedBuilder()
       .setColor("#87ceeb")
       .setTitle("HELP SECTION")      
       .addFields(
-        { name: "Prefix Commands", value: prefixCommands || "No prefix commands available.", inline: false },
-        { name: "Slash Commands", value: slashCommands || "No slash commands available.", inline: false }
+        { name: "Prefix Commands", value: `\`\`\`${prefixCommands}\`\`\`` || "No prefix commands available.", inline: false },
+        { name: "Slash Commands", value: `\`\`\`${slashCommands}\`\`\`` || "No slash commands available.", inline: false }
       );
       
     message.channel.send({ embeds: [embed] });
