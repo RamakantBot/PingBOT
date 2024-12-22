aeb584a06a787ddbfc66bb5a10c7e2c29a0f122e
Ramakant <ramakantbot@duck.com>
2024-12-22 20:02:27 +0530

    add /help cpmmand

---
+++ slash/help.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  description: "Madat karnari command",
+  async execute(message, args) {
+    const prefixCommands = message.client.commands.map(cmd => `${cmd.name}`).join(", ");
+    const slashCommands = message.client.slashCommands.map(cmd => `/${cmd.data.name}`).join(" ");
+
+    const embed = new EmbedBuilder()
+      .setColor("#87ceeb")
+      .setTitle("HELP SECTION")      
+      .addFields(
+        { name: "Prefix Commands", value: `\`\`\`${prefixCommands}\`\`\`` || "No prefix commands available.", inline: false },
+        { name: "Slash Commands", value: `\`\`\`${slashCommands}\`\`\`` || "No slash commands available.", inline: false }
+      );
+      
+    message.channel.send({ embeds: [embed] });
+  },
+};
